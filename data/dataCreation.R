d575032f0355372824caedbe4303a580414aa438
alex <alexander_stephenson@berkeley.edu>
2021-12-21 17:37:17 -0600

    updates and new data

--- data/dataCreation.R
+++ data/dataCreation.R
@@ -10,3 +10,28 @@ occupation <- sample(occupations, 1000, replace = F)
 jobs <- r_data_frame(n = 1000, age, state, income, education, employment, marital, smokes, political, name)%>% 
   mutate(Occupation = occupation,
          across(where(is.factor), as.character))
+
+
+### Animal Rescues in London 
+### Raw data found at https://data.london.gov.uk/dataset/animal-rescue-incidents-attended-by-lfb
+### Licensed by UK Open Government License 
+lfd_raw <- read_csv(here("data/raw_data/lfd_raw.csv"))
+
+animals <- lfd_raw %>%
+  select(IncidentNumber, Year = CalYear, Ward, Borough, 
+         PumpCount, PumpHoursTotal, Type = AnimalGroupParent, 
+         PropertyCategory, RescueType = SpecialServiceTypeCategory,
+         RescueCost = `IncidentNotionalCost(£)`)%>%
+  mutate(Type = case_when(
+    str_detect(Type, "Unknown")~"Unknown",
+    str_detect(Type, "Budgie|Pigeon")~"Bird",
+    str_detect(Type, "cat")~"Cat",
+    str_detect(Type, "Bull")~"Cow",
+    TRUE~Type),
+    Borough = str_to_title(Borough),
+    Ward = str_to_title(Ward),
+    RescueCost = if_else(RescueCost == "NULL", NA_character_, RescueCost),
+    RescueCost = as.numeric(RescueCost))%>%
+  filter(!is.na(Borough))
+
+#write.csv(animals, file = "data/animalRescue.csv", row.names = F)
